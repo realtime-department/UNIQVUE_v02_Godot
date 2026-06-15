5bfbaab51f69c9533cbfd364b31b734653364464
Sebastian Schmitz <schmitz@realtime-department.de>
2026-06-15 16:56:18 +0200

    revert sphere

--- UNIQVUE2-Backgrounds-Godot-Test/particle_wave_root.gd
+++ UNIQVUE2-Backgrounds-Godot-Test/particle_wave_root.gd
@@ -12,17 +12,10 @@ extends Node3D
 			if gb != null:
 				gb.call("set_density", v)
 
-## Kugelradius. Aenderung loest Rebuild + Shader-Sync aus.
-@export_range(50.0, 300.0, 1.0) var sphere_radius: float = 100.0:
-	set(v):
-		sphere_radius = v
-		if is_inside_tree():
-			_apply_sphere_radius()
-
 @export_group("Camera")
-@export_range(0.5, 40.0, 0.5) var cam_height: float = 0.0
-@export_range(20.0, 400.0, 1.0) var cam_dist: float = 220.0
-@export_range(-20.0, 20.0, 0.5) var cam_pitch: float = 0.0  # Y-coord of look target (not angle)
+@export_range(0.5, 40.0, 0.5) var cam_height: float = 3.5
+@export_range(20.0, 160.0, 1.0) var cam_dist: float = 70.0
+@export_range(-20.0, 20.0, 0.5) var cam_pitch: float = 5.0  # Y-coord of look target (not angle)
 @export_range(-40.0, 40.0, 0.5) var cam_yaw: float = 0.0
 @export_range(25.0, 90.0, 1.0) var cam_fov: float = 60.0
 
@@ -30,7 +23,6 @@ extends Node3D
 
 
 func _ready() -> void:
-	_apply_sphere_radius()
 	_update_camera()
 
 
@@ -39,24 +31,13 @@ func _process(_delta: float) -> void:
 	_sync_wire()
 
 
-func _apply_sphere_radius() -> void:
-	var gb := get_node_or_null("Grid")
-	if gb != null:
-		gb.sphere_radius = sphere_radius
-		gb.call("_build")
-	var grid := get_node_or_null("Grid") as MeshInstance3D
-	if grid != null:
-		var gm := grid.material_override as ShaderMaterial
-		if gm != null:
-			gm.set_shader_parameter("sphere_radius", sphere_radius)
-
-
 func _update_camera() -> void:
 	if _camera == null:
 		return
 	_camera.fov = cam_fov
 	_camera.position = Vector3(cam_yaw, cam_height, -cam_dist)
-	_camera.look_at(Vector3(cam_yaw * 0.3, cam_pitch, 0.0), Vector3.UP)
+	# cam_pitch ist die Y-Koordinate des Blickziels (wie HTML: CAM_LOOK.set(camYaw*0.3, camPitch, -60)).
+	_camera.look_at(Vector3(cam_yaw * 0.3, cam_pitch, 60.0), Vector3.UP)
 
 
 ## Geteilte Wellen-Parameter vom Grid-Material auf das Wire-Material uebertragen,
@@ -72,5 +53,5 @@ func _sync_wire() -> void:
 	if gm == null or wm == null:
 		return
 	for p in ["amp", "freq", "wavelength", "speed", "flow", "warp",
-			"dir", "y_off", "mirror", "z_near", "z_far", "sphere_radius"]:
+			"dir", "y_off", "mirror", "z_near", "z_far"]:
 		wm.set_shader_parameter(p, gm.get_shader_parameter(p))
